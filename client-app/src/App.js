101c133c1b516c6c0db204d2a156353e82edbe70
rbtkay <robert.g.khayat@gmail.com>
2020-05-24 18:04:39 +0200

    pages skeletons created

--- client-app/src/App.js
+++ client-app/src/App.js
@@ -2,20 +2,24 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
+import LoginPage from "./pages/LoginPage";
+import HomePage from "./pages/HomePage";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+const App = () => {
+  return (
+    <Router>
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Switch>
+          <Route path="/" exact/>
+          <Route path="/login" component={LoginPage}/>
+          <Route path="/home" exact component={HomePage}/>
+          <Route path="/home/:id" component={HomePage}/>
+        </Switch>
       </div>
-    );
-  }
+    </Router>
+  );
 }
 
 export default App;
