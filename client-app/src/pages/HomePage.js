e5423720cbf9ffc8d1657d9abd74013ce8df6c99
rbtkay <robert.g.khayat@gmail.com>
2020-05-25 17:40:30 +0200

    logout component created

--- client-app/src/pages/HomePage.js
+++ client-app/src/pages/HomePage.js
@@ -5,10 +5,12 @@ import React from 'react';
 import { Header } from "semantic-ui-react";
 
 import Auth from '../components/Auth'
+import NavigationBar from '../components/NavigationBar';
 
 const HomePage = () => {
     return (
         <div>
+            <NavigationBar />
             <Auth />
             <Header as='h1'>Here we will have an input to add new keywords and the list of all the user's keywords</Header>
         </div>
