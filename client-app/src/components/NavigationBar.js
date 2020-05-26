2fc90178e47e9ac5cca2a2a59923e643d90e3ea4
rbtkay <robert.g.khayat@gmail.com>
2020-05-27 00:07:09 +0200

    signup/login on navigation bar

--- client-app/src/components/NavigationBar.js
+++ client-app/src/components/NavigationBar.js
@@ -8,6 +8,10 @@ import Logout from '../components/Logout';
 const NavigationBar = () => {
     const [activeItem, setActiveUser] = useState("editorials")
 
+    const auth_btn_style = {
+        color: "green"
+    }
+
     return (
         <div>
             <Menu>
@@ -17,6 +21,8 @@ const NavigationBar = () => {
                 <Menu.Menu position={'right'}>
                     <Menu.Item>
                         <Switch>
+                            <Route path="/" exact component={() => <Link to={"/register"} style={auth_btn_style}>Sign Up</Link>} />
+                            <Route path="/register" exact component={() => <Link to={"/"} style={auth_btn_style}>Login</Link>} />
                             <Route path="/home" exact component={Logout} />
                         </Switch>
                     </Menu.Item>
@@ -26,8 +32,6 @@ const NavigationBar = () => {
     )
 }
 
-const logout = () => {
 
-}
 
 export default NavigationBar
