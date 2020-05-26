6ad73926c5985a8890b928520cd3151183caf890
rbtkay <robert.g.khayat@gmail.com>
2020-05-27 00:34:25 +0200

    Tweets component created

--- client-app/src/pages/HomePage.js
+++ client-app/src/pages/HomePage.js
@@ -2,17 +2,22 @@ import React from 'react';
 
 // import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import { Header } from "semantic-ui-react";
+import { Header, List } from "semantic-ui-react";
 
 import Auth from '../components/Auth'
 import NavigationBar from '../components/NavigationBar';
+import Tweets from "../components/Tweets";
 
 const HomePage = () => {
+
+    //The component Tweets take in an array and render a list of tweets.
+
     return (
         <div>
             <NavigationBar />
             <Auth />
             <Header as='h1'>Here we will have an input to add new keywords and the list of all the user's keywords</Header>
+            <Tweets tweets={[1,2,3,4]}/>
         </div>
     );
 }
