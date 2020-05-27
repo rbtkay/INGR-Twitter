1fb7b80a377dc09d63a6399b231c8203ecf81e1a
nolwenn <nolwenn.poilleux@gmail.com>
2020-05-27 15:07:25 +0200

    update and delete keyword

--- src/src/Controller/KeywordController.php
+++ src/src/Controller/KeywordController.php
@@ -12,9 +12,8 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class KeywordController extends AbstractController
 {
-
 	/**
-	 * @Route("/api/keywords", name="keyword_create", methods={"POST"})
+	 * @Route("/api/keywords", name="create_keyword", methods={"POST"})
 	 * @param Request $request
 	 * @param KeywordRepository $k_repo
 	 * @return JsonResponse
@@ -93,7 +92,44 @@ class KeywordController extends AbstractController
 	}
 
 	/**
-	 * @Route("/api/keywords/{id}", name="keyword_delete", methods={"DELETE"})
+	 * @Route("/api/keywords/{id}", name="update_keyword", methods={"PUT"})
+	 * @param $id
+	 * @param Request $request
+	 * @param KeywordRepository $k_repo
+	 * @return JsonResponse
+	 */
+	public function updateKeyword($id, Request $request, KeywordRepository $k_repo)
+	{
+		try {
+			$data = json_decode($request->getContent(), true, 512, JSON_THROW_ON_ERROR);
+		} catch (JsonException $e) {
+			return new JsonResponse(
+				['error' => $e->getCode(), 'message' => $e->getMessage()],
+				Response::HTTP_BAD_REQUEST
+			);
+		}
+
+		$keyword = $k_repo->find($id);
+		if (empty($keyword)) {
+			return new JsonResponse(['message' => 'Wrong id'], Response::HTTP_NOT_FOUND);
+		}
+
+		$updated_rows = $k_repo->update($keyword, $data);
+
+		$rows_length = count($updated_rows);
+		if (!$rows_length) {
+			return new JsonResponse(['message' => 'No data to update'], Response::HTTP_BAD_REQUEST);
+		}
+
+		$updated_rows_string = implode(',', $updated_rows);
+		return new JsonResponse(
+			['message' => "Item $updated_rows_string update"],
+			Response::HTTP_OK
+		);
+	}
+
+	/**
+	 * @Route("/api/keywords/{id}", name="delete_keyword", methods={"DELETE"})
 	 * @param $id
 	 * @param Request $request
 	 * @param KeywordRepository $k_repo
--- src/src/Repository/KeywordRepository.php
+++ src/src/Repository/KeywordRepository.php
@@ -38,6 +38,31 @@ class KeywordRepository extends ServiceEntityRepository
 		$this->_em->flush();
 	}
 
+	/**
+	 * @param Keyword $keyword
+	 * @param array $data
+	 * @return array
+	 * @throws \Doctrine\ORM\ORMException
+	 * @throws \Doctrine\ORM\OptimisticLockException
+	 */
+	public function update(Keyword $keyword, array $data)
+	{
+		$updated_rows = [];
+		if (!empty($data['name'])) {
+			$keyword->setName($data['name']);
+			$updated_rows[] = 'username';
+		}
+
+		$this->_em->persist($keyword);
+		$this->_em->flush();
+		return $updated_rows;
+	}
+
+	/**
+	 * @param Keyword $keyword
+	 * @throws \Doctrine\ORM\ORMException
+	 * @throws \Doctrine\ORM\OptimisticLockException
+	 */
 	public function delete(Keyword $keyword)
 	{
 		$this->_em->remove($keyword);
