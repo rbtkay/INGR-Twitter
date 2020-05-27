81154b78d1953c9ad4217d8fd44492f8e6d1bcf7
rbtkay <robert.g.khayat@gmail.com>
2020-05-27 16:33:52 +0200

    keyword refactored + updateKeywordCommand Created

--- client-app/src/components/Keywords.js
+++ client-app/src/components/Keywords.js
@@ -1,15 +1,36 @@
-import React from 'react'
-import { Label } from "semantic-ui-react";
+import React, { useState, useEffect } from 'react'
+import { Label, Icon } from "semantic-ui-react";
 
-const Keywords = ({ keywords }) => {
-
-    const result = keywords.map((keyword) => {
+const Keywords = ({ keywords, callback }) => {
+    return keywords.map((keyword) => {
         return (
-            <Label as='a'>#corona</Label>
+            <Keyword value={keyword.value} isSelected={keyword.isSelected} callback={callback} />
         )
     });
+}
 
-    return result
+const Keyword = ({ value, isSelected, callback }) => {
+    const [color, setColor] = useState("grey")
+    const [selected, setSelected] = useState(isSelected)
+    
+    const setActive = (value) =>{
+        setSelected(!selected);
+    }
+
+    useEffect(() => {
+        let c = selected ? "blue" : "grey";
+        setColor(c)
+        callback(value);
+        console.log(selected)
+    }, [selected])
+
+
+    return (
+        <Label as='a' onClick={() => setActive(value)} color={color}>{value}
+            {selected && <Icon name='delete' />}
+        </Label>
+
+    )
 }
 
 export default Keywords;
---
+++ src/testScript.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo "hello i'm php the great" >> /var/www/cron.txt;
