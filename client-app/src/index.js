54a5338737b5a21225cc40f4844596ea4396bc17
GeoDaz <gfrydz2@gmail.com>
2020-05-28 09:33:15 +0200

    prepare redux

--- client-app/src/index.js
+++ client-app/src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import { createStore } from "redux";
 import "semantic-ui-css/semantic.min.css";
+import App from "./App";
+import reducer from "./reducer";
+
+const store = createStore(reducer);
 
 ReactDOM.render(<App />, document.getElementById("root"));
