b7a63a9649df5c7901b0f0dfb91e1d9c60042273
rbtkay <robert.g.khayat@gmail.com>
2020-05-28 11:47:02 +0200

    refactor of tweet component

--- client-app/src/components/Tweets.js
+++ client-app/src/components/Tweets.js
@@ -1,25 +1,31 @@
-import React from 'react'
-import { List } from "semantic-ui-react";
+import React from "react";
+import {
+  List,
+  ListItem,
+  ListContent,
+  ListHeader,
+  ListIcon,
+  ListDescription,
+} from "semantic-ui-react";
 
 const Tweets = ({ tweets }) => {
-
-    const result = tweets.map((tweet) => {
-        return (
-            <List.Item>
-                <List.Icon name='twitter' size='large' verticalAlign='middle' />
-                <List.Content>
-                    <List.Header as='a'>Semantic-Org/Semantic-UI</List.Header>
-                    <List.Description as='a'>Updated 10 mins ago</List.Description>
-                </List.Content>
-            </List.Item>
-        )
-    });
-
+  const result = tweets.map((tweet) => {
     return (
-        <List divided relaxed>
-            {result}
-        </List>
-    )
-}
+      <ListItem>
+        <ListIcon name="twitter" size="large" verticalAlign="middle" />
+        <ListContent>
+          <ListHeader as="a">Semantic-Org/Semantic-UI</ListHeader>
+          <ListDescription as="a">Updated 10 mins ago</ListDescription>
+        </ListContent>
+      </ListItem>
+    );
+  });
+
+  return (
+    <List divided relaxed>
+      {result}
+    </List>
+  );
+};
 
 export default Tweets;
