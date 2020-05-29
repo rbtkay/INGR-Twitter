3474e29fd54af96738f53bedb48c32f8a48d7171
Ines <jebli1121@gmail.com>
2020-05-29 15:09:15 +0200

    Tweet table

---
+++ src/src/Repository/TweetRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Tweet;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method Tweet|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Tweet|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Tweet[]    findAll()
+ * @method Tweet[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class TweetRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Tweet::class);
+    }
+
+    // /**
+    //  * @return Tweet[] Returns an array of Tweet objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('t')
+            ->andWhere('t.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('t.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?Tweet
+    {
+        return $this->createQueryBuilder('t')
+            ->andWhere('t.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
