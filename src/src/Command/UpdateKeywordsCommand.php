84bdd8525d95269573a52ef15340416194e2c93e
rbtkay <robert.g.khayat@gmail.com>
2020-05-29 16:00:58 +0200

    command getting the users

--- src/src/Command/UpdateKeywordsCommand.php
+++ src/src/Command/UpdateKeywordsCommand.php
@@ -2,6 +2,7 @@
 
 namespace App\Command;
 
+use App\Repository\UserRepository;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -12,6 +13,15 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 class UpdateKeywordsCommand extends Command
 {
     protected static $defaultName = 'update:keywords';
+
+    private $u_repo;
+
+    public function __construct(UserRepository $u_repo)
+    {
+        $this->u_repo = $u_repo;
+
+        parent::__construct();
+    }
 
     protected function configure()
     {
@@ -34,11 +44,17 @@ class UpdateKeywordsCommand extends Command
         if ($input->getOption('option1')) {
             // ...
         }
+
+        $users  = $this->u_repo->findAll();
+
+        foreach ($users as $user){
+            $twitter_name = $user->getTwitterName();
+            
+        }
         $myfile = fopen("../../var/www/result.txt", "a") or die("Unable to open file!");
         $txt = "The Cron is working \n";
         fwrite($myfile, $txt);
-
-        $io->success('Working Command');
+        $io->success('Command Completed');
 
         return 0;
     }
