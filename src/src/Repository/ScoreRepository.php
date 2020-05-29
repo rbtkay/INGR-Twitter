43aa2b2e9056c403bbfb367f4f14fd2c1e4e138a
nolwenn <nolwenn.poilleux@gmail.com>
2020-05-29 16:29:45 +0200

    ScoreRepository

---
+++ src/src/Repository/ScoreRepository.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Score;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method Score|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Score|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Score[]    findAll()
+ * @method Score[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class ScoreRepository extends ServiceEntityRepository
+{
+	/**
+	 * ScoreRepository constructor.
+	 * @param ManagerRegistry $registry
+	 */
+	public function __construct(ManagerRegistry $registry)
+	{
+		parent::__construct($registry, Score::class);
+	}
+}
