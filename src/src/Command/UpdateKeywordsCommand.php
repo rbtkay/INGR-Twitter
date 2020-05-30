877c4f487ffd49aeea135a50905622e918c1d071
rbtkay <robert.g.khayat@gmail.com>
2020-05-31 00:21:07 +0200

    verify if tweeter still exists and is not deleted

--- src/src/Command/UpdateKeywordsCommand.php
+++ src/src/Command/UpdateKeywordsCommand.php
@@ -3,6 +3,7 @@
 namespace App\Command;
 
 use App\Entity\Tweet;
+use App\Entity\User;
 use App\Repository\UserRepository;
 use App\Repository\TweetRepository;
 use Symfony\Component\Console\Command\Command;
@@ -34,8 +35,7 @@ class UpdateKeywordsCommand extends Command
         $this
             ->setDescription('Add a short description for your command')
             ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
-            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
-        ;
+            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
@@ -51,34 +51,80 @@ class UpdateKeywordsCommand extends Command
             // ...
         }
 
-        $users  = $this->u_repo->findAll();
+        $users = $this->u_repo->findAll();
         $io->success($users[0]->getUsername());
 
         $dotenv = new Dotenv();
         $dotenv->load('/var/www/.env');
 
-
-        $url ="https://api.twitter.com/1.1/statuses/user_timeline.json";
+        $url = "https://api.twitter.com/1.1/statuses/user_timeline.json";
 
         $connection = new TwitterOAuth($_ENV["CONSUMER_KEY"], $_ENV["CONSUMER_SECRET"], $_ENV["TWITTER_API_ACCESS_TOKEN"], $_ENV["TWITTER_API_ACCESS_TOKEN_SECRET"]);
-        $tweets = $connection->get("statuses/user_timeline", ["screen_name" => "ingr_project"]);
 
-
-        foreach ($tweets as $tweet){
-//            gettype($tweet);
-//            $io->success($tweet->id);
-//            $io->success($tweet->text);
-//            $io->success($tweet->created_at);
-            //insert into tweets in the database
-            $this->t_repo->insert($tweet->id, $tweet->text, $tweet->created_at, $users[0]->getTwitterName(), $users[0]);
-//            dd($tweet, gettype($tweet));
-//            $io->success(gettype($tweet->id));
+        foreach ($users as $user) {
+            $tweets = $connection->get("statuses/user_timeline", ["screen_name" => $user->getTwitterName()]);
+            $this->addTweets($tweets, $user);
+            $this->deleteOldTweets($tweets, $user);
         }
-        $myfile = fopen("../../var/www/result.txt", "a") or die("Unable to open file!");
-        $txt = "The Cron is working \n";
-        fwrite($myfile, $txt);
-        $io->success('Command Completed');
 
+        $io->success('Command Completed');
         return 0;
+    }
+
+
+    private function addTweets(array $tweets, User $user)
+    {
+        foreach ($tweets as $tweet) {
+            $tweet_result = $this->t_repo->findOneById($tweet->id);
+            if (is_null($tweet_result)) {
+                $this->t_repo->insert($tweet->id, $tweet->text, $tweet->created_at, $user->getTwitterName(), $user);
+            }
+        }
+    }
+
+    private function deleteOldTweets(array $tweets, User $user)
+    {
+        $user_tweets = $this->t_repo->findTweetsByUser($user->getId());
+//        $tweets_diff = array_diff_assoc($user_tweets, $tweets);
+//        $tweets_diff = array_udiff($user_tweets, $tweets,
+//            function ($user_tweet, $tweet) {
+//                return $user_tweet-> - $tweet->tweet;
+//            }
+//        );
+        $tweets_ids = array_map(function ($tweet) {
+            return $tweet->id;
+        }, $tweets);
+//        $user_tweets = array_column($user_tweets, null, "twitter_id");
+//        dump($user_tweets);
+        foreach ($user_tweets as $user_tweet) {
+            // Use the propertyName value from array1 to find details
+//            dump($tweet);
+            $is_still_here = in_array($user_tweet->getTwitterId(), $tweets_ids);
+            dump($is_still_here);
+            if(!$is_still_here){
+                //delete tweet from database;
+            }
+//            dump ($user_tweets[$tweet->id]);
+        }
+//
+//        if(count($tweets_diff) > 0){
+//        foreach ($tweets as $tweet){
+//            dump($tweet->id);
+//        }
+//        }
+        echo "Syccess";
+//        dump("Delete Success");
+//        dump($user_tweets);
+//        foreach ($tweets as $tweet) {
+//            $tweet_result = $this->t_repo->findOneById($tweet->id);
+//            if (is_null($tweet_result)) {
+//                $this->t_repo->insert($tweet->id, $tweet->text, $tweet->created_at, $user->getTwitterName(), $user);
+//            }
+//        }
+    }
+
+    function getId()
+    {
+
     }
 }
