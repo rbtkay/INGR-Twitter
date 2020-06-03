f2492bd8914f36778516109ae5abd75f50f67438
rbtkay <robert.g.khayat@gmail.com>
2020-06-03 20:34:53 +0200

    entrypoint and dockerfile finalized

--- docker/php-fpm/entrypoint.sh
+++ docker/php-fpm/entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-openssl genrsa -out config/jwt/private.pem -aes256 -passout pass:$JWT_PASSPHRASE 4096
-openssl rsa -passin pass:$JWT_PASSPHRASE -pubout -in config/jwt/private.pem -out config/jwt/public.pem
-chmod 644 config/jwt/private.pem
+if ! test -f "config/jwt/private.pem" || ! test -f "config/jwt/public.pem"; then
+    openssl genrsa -out config/jwt/private.pem -aes256 -passout pass:$JWT_PASSPHRASE 4096
+    openssl rsa -passin pass:$JWT_PASSPHRASE -pubout -in config/jwt/private.pem -out config/jwt/public.pem
+    chmod 644 config/jwt/private.pem
+fi
+
 composer install
 crond
 php-fpm
