76c0f517bedcc87ebbe06f97012556cb982576c2
rbtkay <robert.g.khayat@gmail.com>
2020-06-08 23:00:35 +0200

    cleanup before push

--- docker/php-fpm/cron-update.sh
+++ docker/php-fpm/cron-update.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-echo "test the great shell --> " $(date)  >> /var/www/test-result/result.txt
-
 (cd /var/www/; bin/console update:keyword)
 
 echo "The Cron is executed at " $(date) >> /var/www/test-result/result.txt
